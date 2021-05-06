88ef4aebfb9bd79a1afcb1e325786f56b7df3e72
Tamu <5020311+Tamu@users.noreply.github.com>
2021-05-06 13:55:37 +0200

    Add shutdown

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,6 @@
 import { DecoRoutes, datastore, Log, CorsMiddleware, ErrorsMiddleware  } from 'deco-api';
-import { BcfRoutes } from 'deco-bcf';
-import { ThreeRoutes } from 'deco-three';
+import { BcfRoutes } from 'deco-bcf';
+import { ThreeRoutes } from 'deco-three';
 import express from 'express';
 import compression from 'compression';
 import { AnalyticsController, ProxyController } from './controllers';
@@ -15,14 +15,12 @@ let debug = require('debug')('app:server');
 let env = process.env.NODE_ENV || "development";
  
 
-let dbhost = (process.env.DBHOST) || 'localhost';
-let dbuser = (process.env.DBUSER) || '';
-let dbpass = (process.env.DBPASS) || '';
-let dbport = (process.env.DBPORT) || '27017';
-let databaseName = (process.env.DBNAME) || 'sdio';
-const shoudlPrefixProdDb = process.env.DBPREFIXPROD ? true : false;
-let database = (env === 'production' && shoudlPrefixProdDb) ? `${databaseName}prod` : `${databaseName}`;
-datastore.init({database: database, host: dbhost, user: dbuser, password: dbpass, port: dbport}).connect().then(() => {
+let dbhost = (process.env.DBHOST) || 'localhost';
+let dbuser = (process.env.DBUSER) || '';
+let dbpass = (process.env.DBPASS) || '';
+let dbport = (process.env.DBPORT) || '27017';
+let databaseName = (process.env.DBNAME) || 'sdio';
+datastore.init({database: databaseName, host: dbhost, user: dbuser, password: dbpass, port: dbport}).connect().then(() => {
     const app: express.Application = express();
     app.use(express.json({limit: '10mb'}));
 
@@ -50,10 +48,43 @@ datastore.init({database: database, host: dbhost, user: dbuser, password: dbpass
     app.use(ErrorsMiddleware.convertToJsonOutput);
 
     // Serve the application at the given port
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
+        console.log('App started')
         // Success callback
         debug(`Environment: ${env}`);
         debug(`Listening at http://0.0.0.0:${port}/`);
     });
+
+    process.on('SIGTERM', function onSigterm () {
+        shutdown()
+    })
+    process.on('SIGINT', function onSigterm () {
+        shutdown()
+    })
+    process.on('SIGSHUT', function onSigterm () {
+        shutdown()
+    })
+
+    function shutdown() {
+        console.info('Graceful shutdown start', new Date().toISOString());
+        datastore.close().then(() => {
+            console.info('Database closed');
+        }).catch((error) => {
+            console.info('Failed to close database');
+            console.error(error);
+        }).finally(() => {
+            server.close(function onServerClosed (err) {
+                if (err) {
+                    console.info('Failed to close server');
+                    console.error(err)
+                    process.exit(1)
+                } else {
+                    console.info('Closed server');
+                    process.exit()
+                }
+            })
+        });
+        
+    }
 
 });
